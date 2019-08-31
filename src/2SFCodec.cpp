4baa5879a3e3c1032c47fde9477b64c49aefef75
Alwin Esch <alwin.esch@web.de>
2019-08-31 22:09:34 +0200

    remove not needed virtual from class

--- src/2SFCodec.cpp
+++ src/2SFCodec.cpp
@@ -421,7 +421,7 @@ class ATTRIBUTE_HIDDEN C2SFCodec : public kodi::addon::CInstanceAudioDecoder
 {
 public:
   C2SFCodec(KODI_HANDLE instance) :
-    CInstanceAudioDecoder(instance) {}
+    CInstanceAudioDecoder(instance) { }
 
   virtual ~C2SFCodec()
   {
@@ -429,11 +429,11 @@ public:
       state_deinit(&ctx.emu);
   }
 
-  virtual bool Init(const std::string& filename, unsigned int filecache,
-                    int& channels, int& samplerate,
-                    int& bitspersample, int64_t& totaltime,
-                    int& bitrate, AEDataFormat& format,
-                    std::vector<AEChannel>& channellist) override
+  bool Init(const std::string& filename, unsigned int filecache,
+            int& channels, int& samplerate,
+            int& bitspersample, int64_t& totaltime,
+            int& bitrate, AEDataFormat& format,
+            std::vector<AEChannel>& channellist) override
   {
     ctx.pos = 0;
     if (psf_load(filename.c_str(), &psf_file_system, 0x24, 0,
@@ -473,7 +473,7 @@ public:
     return true;
   }
 
-  virtual int ReadPCM(uint8_t* buffer, int size, int& actualsize) override
+  int ReadPCM(uint8_t* buffer, int size, int& actualsize) override
   {
     if (ctx.pos >= ctx.len)
       return 1;
@@ -483,7 +483,7 @@ public:
     return 0;
   }
 
-  virtual int64_t Seek(int64_t time) override
+  int64_t Seek(int64_t time) override
   {
     if (time*ctx.sample_rate*4/1000 < ctx.pos)
     {
@@ -504,8 +504,8 @@ public:
     return ctx.pos/(ctx.sample_rate*4)*1000;
   }
 
-  virtual bool ReadTag(const std::string& file, std::string& title,
-                       std::string& artist, int& length) override
+  bool ReadTag(const std::string& file, std::string& title,
+               std::string& artist, int& length) override
   {
     TSFContext tsf;
 
@@ -529,15 +529,13 @@ private:
 class ATTRIBUTE_HIDDEN CMyAddon : public kodi::addon::CAddonBase
 {
 public:
-  CMyAddon() { }
-  virtual ADDON_STATUS CreateInstance(int instanceType, std::string instanceID, KODI_HANDLE instance, KODI_HANDLE& addonInstance) override
+  CMyAddon() = default;
+  ADDON_STATUS CreateInstance(int instanceType, std::string instanceID, KODI_HANDLE instance, KODI_HANDLE& addonInstance) override
   {
     addonInstance = new C2SFCodec(instance);
     return ADDON_STATUS_OK;
   }
-  virtual ~CMyAddon()
-  {
-  }
+  virtual ~CMyAddon() = default;
 };
 
 
